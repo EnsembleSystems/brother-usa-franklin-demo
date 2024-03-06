723df23598da05a54208ce227b83b502724e94f9
Max Wong <maxw@MBP-353-maxw.local>
2024-03-06 15:45:12 -0800

    css and link fix

--- blocks/footer-content/footer-content.js
+++ blocks/footer-content/footer-content.js
@@ -2,7 +2,7 @@ export default function decorate(block) {
     const bbbLogo = block.querySelector('img');
     const pictureTag = block.querySelector('picture');
     const bbbLogoLink = document.createElement('a');
-    bbbLogoLink.href = 'https://www.bbb.org/us/nj/bridgewater/profile/commercial-manufacturers/brother-international-corporation-0221-11003469#sealclick';
+    bbbLogoLink.href = `${window.location.origin}/`
     bbbLogoLink.target = '_blank';
     bbbLogoLink.append(bbbLogo);
     pictureTag.append(bbbLogoLink);
